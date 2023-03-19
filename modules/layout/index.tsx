31f451041fb75a1d1c30d5904600a8cd1454c803
Eduardo Carneiro <eduardo.carneiro@farfetch.com>
2023-03-19 21:59:14 +0000

    Fix bug layout

--- modules/layout/index.tsx
+++ modules/layout/index.tsx
@@ -20,7 +20,7 @@ const Layout: FunctionComponent<Props> = ({children}) => {
         <Head>
           <title>Kuirius</title>
           <meta name="description" content="Pelo amor à comida e restauração" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1.0, user-scalable=no" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <div className={styles.container}>
