1db9f8a8c4b45357895c24391071700e7ed59cb2
Eduardo Carneiro <eduardo.carneiro@farfetch.com>
2023-03-25 11:12:26 +0000

    Logo with link to home page

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,5 +1,7 @@
 import type { FunctionComponent } from 'react'
 import styles from './logo.module.scss'
+import { useRouter } from 'next/router'
+
 
 type Props = {
     children: string;
@@ -8,10 +10,16 @@ type Props = {
 
 const Logo: FunctionComponent<Props> = ({children, slogan}) => {
     const styleType = slogan  ? styles.strong : styles.light;
+
+    const router = useRouter();
+
+    const handleHome = () => {
+        router.push('/');
+    }
     
     return  (
         <div className={styles.logo}>
-            <h2 className={styleType}>{children}</h2>
+            <h2 className={styleType} onClick={handleHome}>{children}</h2>
             <h5>{slogan}</h5>
         </div>
     )
